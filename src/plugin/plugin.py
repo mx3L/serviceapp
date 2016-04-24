1b39e7ffd28f05adb6b8a73a263ad969901e2a5f
Taapat <taapat@gmail.com>
2016-04-24 11:06:56 +0300

    plugin: PEP8 python style fixes

--- src/plugin/plugin.py
+++ src/plugin/plugin.py
@@ -7,22 +7,23 @@ from Components.config import config, ConfigSubsection, ConfigSelection, ConfigB
 from Plugins.Plugin import PluginDescriptor
 from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen
-
 from enigma import eEnv
+
 import serviceapp_client
+
 
 SINKS_DEFAULT = ("dvbvideosink", "dvbaudiosink")
 SINKS_EXPERIMENTAL = ("dvbvideosinkexp", "dvbaudiosinkexp")
 
 sinkChoices = []
 if (os.path.isfile(eEnv.resolve("$libdir/gstreamer-1.0/libgstdvbvideosink.so")) and
-    os.path.isfile(eEnv.resolve("$libdir/gstreamer-1.0/libgstdvbaudiosink.so"))):
+			os.path.isfile(eEnv.resolve("$libdir/gstreamer-1.0/libgstdvbaudiosink.so"))):
 	sinkChoices.append("original")
 if (os.path.isfile(eEnv.resolve("$libdir/gstreamer-1.0/libgstdvbvideosinkexp.so")) and
-    os.path.isfile(eEnv.resolve("$libdir/gstreamer-1.0/libgstdvbaudiosinkexp.so"))):
+			os.path.isfile(eEnv.resolve("$libdir/gstreamer-1.0/libgstdvbaudiosinkexp.so"))):
 	sinkChoices.append("experimental")
-	
-playerChoices = [ "gstplayer", "exteplayer3" ]
+
+playerChoices = ["gstplayer", "exteplayer3"]
 
 config.plugins.serviceapp = ConfigSubsection()
 configServiceApp = config.plugins.serviceapp
@@ -37,8 +38,8 @@ configServiceApp.gstplayer["servicemp3"] = ConfigSubsection()
 configServiceApp.gstplayer["servicegstplayer"] = ConfigSubsection()
 for key in configServiceApp.gstplayer.keys():
 	configServiceApp.gstplayer[key].sink = ConfigSelection(default="original", choices=sinkChoices)
-	configServiceApp.gstplayer[key].bufferSize = ConfigInteger(8192, (1024, 1024 * 64)) 
-	configServiceApp.gstplayer[key].bufferDuration = ConfigInteger(0, (0, 100)) 
+	configServiceApp.gstplayer[key].bufferSize = ConfigInteger(8192, (1024, 1024 * 64))
+	configServiceApp.gstplayer[key].bufferDuration = ConfigInteger(0, (0, 100))
 	configServiceApp.gstplayer[key].subtitleEnabled = ConfigBoolean(default=True)
 
 configServiceApp.exteplayer3 = ConfigSubDict()
@@ -66,7 +67,7 @@ def initServiceAppSettings():
 		bufferDuration = playerCfg.bufferDuration.value
 
 		serviceapp_client.setGstreamerPlayerSettings(settingId, videoSink, audioSink, subtitleEnabled, bufferSize, bufferDuration)
-	
+
 	if configServiceApp.servicemp3.player.value == "gstplayer":
 		serviceapp_client.setServiceMP3GstPlayer()
 	elif configServiceApp.servicemp3.player.value == "exteplayer3":
@@ -77,23 +78,22 @@ initServiceAppSettings()
 
 class ServiceAppSettings(Screen, ConfigListScreen):
 	skin = """
-        <screen position="center,center" size="400,400" title="ServiceApp Settings" backgroundColor="#48080808">
-        	<widget name="config" position="10,10" size="380,380" />
-        </screen> """
+		<screen position="center,center" size="400,400" title="ServiceApp Settings" backgroundColor="#48080808">
+			<widget name="config" position="10,10" size="380,380" />
+		</screen> """
+
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		ConfigListScreen.__init__(self, [], session)
 		self.onLayoutFinish.append(self.initConfigList)
 		self.onClose.append(self.deInitConfig)
-		self["actions"] = ActionMap(["SetupActions"], {"ok":self.keyOk, "cancel":self.keyCancel}, -2)
-		
+		self["actions"] = ActionMap(["SetupActions"], {"ok": self.keyOk, "cancel": self.keyCancel}, -2)
 
 	def initConfigList(self):
 		configServiceApp.servicemp3.player.addNotifier(self.serviceMP3PlayerChanged, initial_call=False)
 		configServiceApp.servicemp3.replace.addNotifier(self.serviceMP3ReplacedChanged, initial_call=False)
 		self["config"].setList(self.buildConfigList())
-		
-		
+
 	def buildConfigList(self):
 		configList = [getConfigListEntry(_("Enigma2 playback system"), configServiceApp.servicemp3.replace)]
 		if configServiceApp.servicemp3.replace.value:
@@ -108,7 +108,7 @@ class ServiceAppSettings(Screen, ConfigListScreen):
 				configList.append(getConfigListEntry("  " + _("Buffer duration"), gstPlayerOptionsCfg.bufferDuration))
 			else:
 				configList.append(getConfigListEntry("", ConfigNothing()))
-				configList.append(getConfigListEntry(_("ServiceMp3 (%s)"% str(serviceapp_client.ID_SERVICEMP3)), ConfigNothing()))
+				configList.append(getConfigListEntry(_("ServiceMp3 (%s)" % str(serviceapp_client.ID_SERVICEMP3)), ConfigNothing()))
 		gstPlayerOptionsCfg = configServiceApp.gstplayer["servicegstplayer"]
 		configList.append(getConfigListEntry("", ConfigNothing()))
 		configList.append(getConfigListEntry(_("ServiceGstPlayer (%s)" % str(serviceapp_client.ID_SERVICEGSTPLAYER)), ConfigNothing()))
@@ -119,24 +119,23 @@ class ServiceAppSettings(Screen, ConfigListScreen):
 		configList.append(getConfigListEntry("", ConfigNothing()))
 		configList.append(getConfigListEntry(_("ServiceExtEplayer3 (%s)" % str(serviceapp_client.ID_SERVICEEXTEPLAYER3)), ConfigNothing()))
 		return configList
-	
+
 	def serviceMP3ReplacedChanged(self, configElement):
 		self["config"].setList(self.buildConfigList())
-	
+
 	def serviceMP3PlayerChanged(self, configElement):
 		self["config"].setList(self.buildConfigList())
-		
-			
+
 	def deInitConfig(self):
 		configServiceApp.servicemp3.player.removeNotifier(self.serviceMP3PlayerChanged)
 		configServiceApp.servicemp3.replace.removeNotifier(self.serviceMP3ReplacedChanged)
-		
+
 	def keyOk(self):
 		if configServiceApp.servicemp3.replace.isChanged():
 			self.session.openWithCallback(self.saveSettingsAndClose, MessageBox, _("Enigma2 Playback System was changed and Enigma2 should be restarted\nDo you want to restart it now?"), type=MessageBox.TYPE_YESNO)
 		else:
 			self.saveSettingsAndClose()
-		
+
 	def saveSettingsAndClose(self, callback=False):
 		initServiceAppSettings()
 		if configServiceApp.servicemp3.replace.value:
@@ -145,7 +144,8 @@ class ServiceAppSettings(Screen, ConfigListScreen):
 			serviceapp_client.setServiceMP3Replace(False)
 		self.saveAll()
 		self.close(callback)
-		
+
+
 def main(session, **kwargs):
 	def restartE2(restart=False):
 		if restart:
@@ -153,6 +153,6 @@ def main(session, **kwargs):
 			session.open(TryQuitMainloop, 3)
 	session.openWithCallback(restartE2, ServiceAppSettings)
 
+
 def Plugins(**kwargs):
 	return [PluginDescriptor(name=_("ServiceApp"), description=_("setup player framework"), where=PluginDescriptor.WHERE_PLUGINMENU, needsRestart=False, fnc=main)]
-
