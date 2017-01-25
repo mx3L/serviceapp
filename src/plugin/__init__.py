5bd5db39d68202be1c74972c1def765ee17cddfa
Taapat <taapat@gmail.com>
2017-01-25 21:59:27 +0200

    Improve translation
    Add missing Makefile.am in locale
    Update missing in configure.ac and Makefile.am in src
    Add missing for translation in __init__.py and plugin.py
    Regenerate ServiceApp.pot
    Add Latvian translation

---
+++ src/plugin/__init__.py
@@ -0,0 +1,21 @@
+from os import environ
+from gettext import bindtextdomain, dgettext, gettext
+
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+
+
+def localeInit():
+	environ["LANGUAGE"] = language.getLanguage()[:2]
+	bindtextdomain("ServiceApp", resolveFilename(SCOPE_PLUGINS, \
+		"SystemPlugins/ServiceApp/locale"))
+
+
+def _(txt):
+	t = dgettext("ServiceApp", txt)
+	if t == txt:
+		t = gettext(txt)
+	return t
+
+localeInit()
+language.addCallback(localeInit)
